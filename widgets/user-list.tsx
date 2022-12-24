c694e88ed8c6c5582ab974e1739cd0267ba36010
erwinagpasa <rootaccess000@gmail.com>
2022-12-24 13:52:29 +0300

    fixing menu

--- widgets/user-list.tsx
+++ widgets/user-list.tsx
@@ -33,76 +33,25 @@ export const UserList = (props: { data: any[]; }) => {
                   </Menu.Button>
                 </div>
 
-                <Transition
-                  as={Fragment}
+                <Transition as={Fragment}
                   enter="transition ease-out duration-100"
                   enterFrom="transform opacity-0 scale-95"
                   enterTo="transform opacity-100 scale-100"
                   leave="transition ease-in duration-75"
                   leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
+                  leaveTo="transform opacity-0 scale-95">
+
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Account settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Support
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            License
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <form method="POST" action="#">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              type="submit"
-                              className={classNames(
-                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                'block w-full px-4 py-2 text-left text-sm'
-                              )}
-                            >
-                              Sign out
-                            </button>
-                          )}
-                        </Menu.Item>
-                      </form>
+                      <Menu.Item><a href="/" className="block px-4 py-2 text-sm">Manama Bahrain</a></Menu.Item>
+                      <Menu.Item><a href="/" className="block px-4 py-2 text-sm">GuangZhou China</a></Menu.Item>
+                      <Menu.Item><a href="/" className="block px-4 py-2 text-sm">Yiwu China</a></Menu.Item>
+                      <Menu.Item><a href="/" className="block px-4 py-2 text-sm">Dasmariñas Philippines</a></Menu.Item>
+                      <Menu.Item><a href="/" className="block px-4 py-2 text-sm">Valenzuela Philippines</a></Menu.Item>
                     </div>
                   </Menu.Items>
                 </Transition>
               </Menu>
-
             </div>
 
             <div className="relative pt-3">
@@ -171,17 +120,12 @@ export const UserList = (props: { data: any[]; }) => {
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <Menu.Item>
                               <Link href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                Your Profile
+                                View
                               </Link>
                             </Menu.Item>
                             <Menu.Item>
                               <Link href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                Your Profile
-                              </Link>
-                            </Menu.Item>
-                            <Menu.Item>
-                              <Link href="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                Your Profile
+                                Edit
                               </Link>
                             </Menu.Item>
                           </Menu.Items>
